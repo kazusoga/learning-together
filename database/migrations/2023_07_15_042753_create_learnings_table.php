758424052127f44fd058fc375998937eea31d31b
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-07-15 13:34:38 +0900

    add: migration: learnings テーブルを追加

---
+++ database/migrations/2023_07_15_042753_create_learnings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('learnings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('detail');
+            $table->boolean('helping')->default(false);
+            $table->boolean('recruiting')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('learnings');
+    }
+};
