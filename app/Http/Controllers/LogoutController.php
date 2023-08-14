18b958b8a099fbfd44440f6859c5f36e130f0537
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-08-14 15:23:44 +0900

    fix: LogoutController: ガードを web に指定

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -8,7 +8,9 @@ class LogoutController extends Controller
 {
     public function __invoke(Request $request)
     {
-        auth()->logout();
+        // ログアウト処理
+        auth()->guard("web")->logout();
+
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
